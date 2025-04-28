2535a626e188be18213ed1e1ff6ec67de043a74d
Raj Manandhar <raj.mdhr@gmail.com>
2025-04-28 10:57:13 +0545

    User Model added

--- model/UserModel.js
+++ model/UserModel.js
@@ -1,22 +1,56 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
+const uuidv1 = require("uuidv1");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: Number,
+      default: 0,
+      //  0 is user and 1 is admin
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    salt: String,
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ["customer", "admin"],
-    default: "customer",
-  },
+  { timestamps: true }
+);
+UserSchema.virtual("password").set(function (password) {
+  this._password = password;
+  this.salt = uuidv1();
+  this.hashed_password = this.encryptedPassword(this._password);
 });
+UserSchema.methods = {
+  encryptedPassword: function (password) {
+    if (password == null) {
+      return null;
+    }
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch {
+      return null;
+    }
+  },
+  authenticate: function (password) {
+    return this.hashed_password == this.encryptedPassword(password);
+  },
+};
+module.exports = mongoose.model("User", UserSchema);
